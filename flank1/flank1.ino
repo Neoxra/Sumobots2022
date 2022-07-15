d7c9f6a21de1a3087bfdeb7eb7c55d1ec74ef327
Nick Bell <nickojbell@gmail.com>
2022-07-16 01:59:07 +1000

    Bug fixes and code cleanup

--- flank1/flank1.ino
+++ flank1/flank1.ino
@@ -41,8 +41,10 @@ int motor2pin2 = 52;
 int motor1speed = 12;
 int motor2speed = 13;
 
-// See what state of the game we are in
+// See what state of the game we are in; initial start 0
 int gamestate = 0;
+
+
 void setup() {
   // Setup IO pins
   pinMode(trig_pin_left, OUTPUT); 
@@ -64,19 +66,22 @@ void setup() {
 
 }
 
+
+
 void loop() {
   // Wait for button to be pressed and then wait 5 seconds before start
 
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
   
-  // Step 1 move
+  // Step 1 move forward
   long int t1 = millis();
   long int t2 = millis();
+
   if (gamestate == 1){
     // Take current runtime
-    t1 = millis();
-  
+    t1 = millis(); 
     t2 = millis();
+
     while (t2 <= (t1 + 1000) && check_any_line == 0) {
       
       analogWrite(motor1speed, 255); 
@@ -93,7 +98,7 @@ void loop() {
     
   }
 
-  // Step 2 turn left and find
+  // Step 2 turn left and find enemy
   if (gamestate == 2) {
 
       analogWrite(motor1speed, 255); 
@@ -113,7 +118,7 @@ void loop() {
       
   }
 
-  // Trun right until we can no longer see the oponent and then drive forward a bit
+  // Turn right until we can no longer see the oponent and then drive forward a bit
   if (gamestate == 3) {
 
       analogWrite(motor1speed, 255); 
@@ -124,22 +129,22 @@ void loop() {
       // Check if the oponent is no longer directly in front of us or that we aren't over the line
       if ((evaluate_distance(trig_pin_left, echo_pin_left) > 80 && evaluate_distance(trig_pin_right, echo_pin_right) > 80) && check_any_line == 0) {
       
-      t1 = millis();
-  
-      t2 = millis();
-      while (t2 <= (t1 + 1000) && ) {
-        
-        analogWrite(motor1speed, 255); 
-        analogWrite(motor2speed, 255); 
-        digitalWrite(motor1pin1, HIGH);
-        digitalWrite(motor1pin2, HIGH);
-        
+        t1 = millis();  
         t2 = millis();
-      }
-      
-      analogWrite(motor1speed, 0); 
-      analogWrite(motor2speed, 0); 
-      gamestate = 4;
+
+        while (t2 <= (t1 + 1000) && check_any_line == 0) {
+          
+          analogWrite(motor1speed, 255); 
+          analogWrite(motor2speed, 255); 
+          digitalWrite(motor1pin1, HIGH);
+          digitalWrite(motor1pin2, HIGH);
+          
+          t2 = millis();
+        }
+        
+        analogWrite(motor1speed, 0); 
+        analogWrite(motor2speed, 0); 
+        gamestate = 4;
     }
   }
 
@@ -199,9 +204,9 @@ int evaluate_distance(int trigPin, int echoPin) {
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
+  int duration = pulseIn(echoPin, HIGH);
   // Calculating the distance
-  distance = duration * 0.034 / 2;
+  int distance = duration * 0.034 / 2;
   // Prints the distance on the Serial Monitor
   Serial.print("Distance: ");
   Serial.println(distance);
@@ -213,16 +218,16 @@ int evaluate_distance(int trigPin, int echoPin) {
 int check_any_line() {
   // Check if any line sensor has seen the line
   if (digitalRead(top_left) == white) {
-    return 1
+    return 1;
   }
   if (digitalRead(top_left) == white) {
-    return 2
+    return 2;
   }
   if (digitalRead(top_left) == white) {
-    return 3
+    return 3;
   }
   if (digitalRead(top_left) == white) {
-    return 4
+    return 4;
   }
 
   return 0;
